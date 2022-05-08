5e59fdefc7df5fa115c9113580733a05695ff5e3
Fred Snyder <safe.sand1626@fastmail.com>
2022-05-08 19:34:39 +0200

    Setup Express views with EJS

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// setup EJS views
+app.set('views', './views');
+app.set('view engine', 'ejs');
 app.get('/', (req, res, next) => {
 	res.send('Hello');
 });
